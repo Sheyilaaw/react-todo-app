f10d8517d2985beca825597b20c6e9830cccaddc
Sheyilaaw <sheyilaaw98@gmail.com>
2018-11-10 00:15:37 +0100

    Added feature to display todos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from "./Todos";
 
 class App extends Component {
+
+  state = {
+      todos:[
+          {id: 1, content: 'Buy Milk'},
+          {id: 2, content: 'Play COD'}
+      ]
+  };
+
+  deleteTodo = (id) => {
+      const todos = this.state.todos.filter(todo => {
+         return todo.id !== id;
+      });
+      this.setState({
+          todos: todos
+      })
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return (
+        <div className="todo-app container">
+            <h1 className="center blue-text">Todo's</h1>
+            <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        </div>
+      );
   }
 }
 
