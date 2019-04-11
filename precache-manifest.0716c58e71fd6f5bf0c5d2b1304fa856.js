136ed93ff73e3018f33c354f4e565b25a9672c30
Sheyilaaw <sheyilaaw98@gmail.com>
2019-04-11 10:09:05 +0100

    Updates

---
+++ precache-manifest.0716c58e71fd6f5bf0c5d2b1304fa856.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "da6aae320b5c9e3feaca",
+    "url": "/react-todo-app/static/js/main.da6aae32.chunk.js"
+  },
+  {
+    "revision": "b36c5939986e1f29f72d",
+    "url": "/react-todo-app/static/js/1.b36c5939.chunk.js"
+  },
+  {
+    "revision": "da6aae320b5c9e3feaca",
+    "url": "/react-todo-app/static/css/main.d215ce5b.chunk.css"
+  },
+  {
+    "revision": "f5f7ad6a8b59bf5b9318a11ad8633072",
+    "url": "/react-todo-app/index.html"
+  }
+];
